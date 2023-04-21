5ee53c7b69cc694c1682e39225424f1d976d2370
musa <mnzawanga43king@gmail.com>
2023-04-21 09:31:29 -0700

    file commited succesfully. SAVE POINT SUCCESS

---
+++ src/user.entity.ts
@@ -0,0 +1,14 @@
+import { PrimaryGeneratedColumn, Column, Entity } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+
+    id:number;
+
+    @Column()
+    name:string;
+
+    @Column()
+    surname:string;
+}
